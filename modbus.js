dbe71afdb5f96c6147178d056aeb7b66aa9bb9a5
nimagrolis <486016@gmail.com>
2019-01-31 15:51:14 +0300

    v0.0.2

--- modbus.js
+++ modbus.js
@@ -4,13 +4,12 @@
  */
 const util = require("util");
 
-const logger = require("./lib/logger");
 const plugin = require("./lib/plugin");
 const agent = require("./lib/agent");
 
 plugin.unitId = process.argv[2];
 
-logger.log("Plugin " + plugin.unitId + " has started.", "connect");
+plugin.log("Plugin " + plugin.unitId + " has started.", "connect");
 
 let step = 0;
 next();
@@ -31,7 +30,7 @@ function next() {
 
     case 2:
       // Подключение
-      agent.start(plugin, logger);
+      agent.start(plugin);
       // setInterval(checkResponse, 1000);
       step = 3;
       break;
@@ -48,14 +47,14 @@ function getTable(name) {
 function checkResponse() {
   let res = agent.checkResponse();
   if (res) {
-    logger.log(res);
+    plugin.log(res);
     agent.stop();
     process.exit(2);
   }
 }
 */
 
-/******************************** Входящие от IH ****************************************************/
+/** ****************************** Входящие от IH ****************************************************/
 process.on("message", message => {
   if (!message) return;
   if (typeof message == "string") {
@@ -74,10 +73,10 @@ function parseMessageFromServer(message) {
     case "get":
       if (message.params) {
         plugin.setParams(message.params);
-        if (message.params.debug) logger.setDebug(message.params.debug);
+        if (message.params.debug) plugin.setDebug(message.params.debug);
         // ПОКА
         if ( plugin.params.transport != 'tcp') {
-            logger.log("Транспорт в текущей версии не поддерживается!");
+            plugin.log("Транспорт в текущей версии не поддерживается!");
             process.exit(0);
         }
         next();
@@ -93,7 +92,7 @@ function parseMessageFromServer(message) {
       break;
 
     case "debug":
-      if (message.mode) logger.setDebug(message.mode);
+      if (message.mode) plugin.setDebug(message.mode);
       break;
 
     default:
@@ -105,13 +104,13 @@ function doAct(data) {
   if (!data || !util.isArray(data) || data.length <= 0) return;
 
   if (step < 3) {
-    logger.log("Init operation. Skip command");
+    plugin.log("Init operation. Skip command");
     return;
   }
 
   data.forEach(item => {
     if (item.id && item.command) {
-      // agent.doCommand(item);
+      agent.doCommand(item);
       // и на сервер передать что сделали? или придет самотеком?
       // plugin.sendDataToServer([{ id: item.id, value }]);
     }
@@ -120,7 +119,7 @@ function doAct(data) {
 
 process.on("uncaughtException", err => {
   var text = "ERR (uncaughtException): " + util.inspect(err);
-  logger.log(text);
+  plugin.log(text);
 });
 
 process.on("disconnect",() => {
